14c11362ca361fd7f6e32307991a3f23476e91b8
Jack Copsey <jack.copsey@yahoo.com>
2020-12-10 08:12:39 +0000

    Replace main.m with AppDelegate.swift

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//
+//  Created by Jack Copsey on 10/12/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet var window: NSWindow!
+
+//    func applicationDidFinishLaunching(_ notification: Notification) {
+//        // Insert code here to initialize your application
+//    }
+
+//    func applicationWillTerminate(_ notification: Notification) {
+//        // Insert code here to tear down your application
+//    }
+}
